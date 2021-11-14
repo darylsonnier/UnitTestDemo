18f62b089e85742ba64633cdf24921c74c04179a
Daryl Sonnier <daryl@chaosgamingpc.com>
2021-11-14 11:05:39 -0600

    Initial Commit
    Initial Commit

---
+++ UnitTestDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ UnitTestDemoTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Calculator calc = new Calculator();
+            double sum = calc.Sum(2, 4);
+            Assert.AreEqual(sum, 6);
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            Calculator calc = new Calculator();
+            double dif = calc.Sub(4, 2);
+            Assert.AreEqual(dif, 2);
+        }
+
+        [TestMethod()]
+        public void MulTest()
+        {
+            Calculator calc = new Calculator();
+            double prod = calc.Mul(2, 2);
+            Assert.AreEqual(prod, 4);
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            Calculator calc = new Calculator();
+            double quo = calc.Div(6, 3);
+            Assert.AreEqual(quo, 2);
+        }
+    }
+}
