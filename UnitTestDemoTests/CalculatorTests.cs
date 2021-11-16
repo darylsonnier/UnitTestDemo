9dc4c9db1e7cbfe259aaef85f474765768cb82d5
Daryl Sonnier <daryl@chaosgamingpc.com>
2021-11-16 16:20:24 -0600

    Update comments
    Add periods at end of sentences

--- UnitTestDemoTests/CalculatorTests.cs
+++ UnitTestDemoTests/CalculatorTests.cs
@@ -15,7 +15,7 @@ namespace UnitTestDemo.Tests
         public void SumTest()
         {
             Calculator calc = new Calculator();
-            // Sum of 2 and 4 is 6
+            // Sum of 2 and 4 is 6.
             double sum = calc.Sum(2, 4);
             Assert.AreEqual(sum, 6);
         }
@@ -24,7 +24,7 @@ namespace UnitTestDemo.Tests
         public void SubTest()
         {
             Calculator calc = new Calculator();
-            // Difference of 4 and 2 is 2
+            // Difference of 4 and 2 is 2.
             double dif = calc.Sub(4, 2);
             Assert.AreEqual(dif, 2);
         }
@@ -33,7 +33,7 @@ namespace UnitTestDemo.Tests
         public void MulTest()
         {
             Calculator calc = new Calculator();
-            // Product of 2 and 2 is 4
+            // Product of 2 and 2 is 4.
             double prod = calc.Mul(2, 2);
             Assert.AreEqual(prod, 4);
         }
@@ -42,7 +42,7 @@ namespace UnitTestDemo.Tests
         public void DivTest()
         {
             Calculator calc = new Calculator();
-            // Quotient of 6 and 3 is 2
+            // Quotient of 6 and 3 is 2.
             double quo = calc.Div(6, 3);
             Assert.AreEqual(quo, 2);
         }
